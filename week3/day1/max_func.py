958eae59fc8ac0fa4cea8f568bb752bece1d9201
Pola-Gur <pola.gur.il@gmail.com>
2024-06-16 11:51:19 +0300

    exrcisesXP only first exercise

---
+++ week3/day1/max_func.py
@@ -0,0 +1,9 @@
+numbers = [4, 2, 5, 6, 7, 8]
+
+largest_number = max[numbers]
+
+print(largest_number)
+
+
+words = ["abvc", "sdfjdskfh", "dsajflk ", "abvc rtpo"]
+# need to complete after class record realize
