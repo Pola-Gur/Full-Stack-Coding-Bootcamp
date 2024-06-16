c1fd65519e4fdf1a364c01e50b38e2042e4c7610
Pola-Gur <pola.gur.il@gmail.com>
2024-06-16 11:52:37 +0300

    moved func file to another folder (out of repo)

--- week3/day1/max_func.py
+++
@@ -1,9 +0,0 @@
-numbers = [4, 2, 5, 6, 7, 8]
-
-largest_number = max[numbers]
-
-print(largest_number)
-
-
-words = ["abvc", "sdfjdskfh", "dsajflk ", "abvc rtpo"]
-# need to complete after class record realize
