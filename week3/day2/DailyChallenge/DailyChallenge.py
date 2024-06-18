9c60e8a24185adee03c93212d5ae3a73becbf342
Pola-Gur <pola.gur.il@gmail.com>
2024-06-18 22:19:17 +0300

    fixed getVisibleItems

--- week3/day2/DailyChallenge/DailyChallenge.py
+++ week3/day2/DailyChallenge/DailyChallenge.py
@@ -34,42 +34,48 @@
 # Please set the p.totalPages and p.currentPage attributes to the appropriate number as there cannot be a page 0.
 # If a page is outside of the totalPages attribute, then the goToPage method should go to the closest page to the number provided (e.g. there are only 5 total pages, but p.goToPage(10) is given: the p.currentPage should be set to 5; if 0 or a negative number is given, p.currentPage should be set to 1).
 
+from typing import Optional
+from math import ceil
+
+
 class Pagination:
-    def __init__(self, items: list = None, pageSize: int = 10):
+    def __init__(self, items: Optional[list], pageSize: int = 10) -> None:
          self.items = items
          self.pageSize = pageSize
-         self.totalPages = len(items)
+         self.totalPages = ceil(len(items) / pageSize)  
          self.currentPage = 1
 
-    def getVisibleItems(self, start: int = 0):
-        self.start = start
-        if 
-        print(self.items[start: start + self.pageSize])
+    def getVisibleItems(self) -> list:
+        end_idx = self.currentPage * self.pageSize  # 2 * 4 -> 8
+        start_idx = end_idx - self.pageSize  # 8 - 4 = 4
+        return self.items[start_idx:end_idx]
     
-    # def prevPage(self):
-        
+    def prevPage(self):
+        self.goToPage(self.currentPage - 1)
 
-    # def nextPage(self):
-        
-         
-    # def firstPage(self):
-         
+    def nextPage(self):
+        self.goToPage(self.currentPage + 1)
+
+    def firstPage(self):
+        self.goToPage(1)
 
     def lastPage(self):
-        self.start = -(self.totalPages % self.pageSize)
-        self.currentPage = self.totalPages
-        return self.start, self.totalPages
-    
-    def goToPage(self, pageNum):
-        self.pageNum = pageNum
-        if pageNum > self.totalPages:
-            pageNum = self.totalPages
-        if pageNum < 1:
-            pageNum = 1
+        self.goToPage(self.totalPages) 
 
-alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+    def goToPage(self, pageNum: int):
+        if pageNum <= 0 or pageNum > self.totalPages:
+            raise ValueError(
+                "page_num isn't in the correct range of the available pages"
+            )
 
-p = Pagination(alphabetList, 4)
-p.getVisibleItems() 
-p.lastPage()
+def main():
+    alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+    p = Pagination(alphabetList, 4)
 
+    p.goToPage(1)
+    p.lastPage()
+    current_items = p.getVisibleItems()
+    print(current_items)
+
+if __name__ == "__main__":
+    main()
