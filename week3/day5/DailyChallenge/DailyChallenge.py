9488fa5ccce0d5e9a89c99ebaaa6ec86d56aa3f6
Pola-Gur <pola.gur.il@gmail.com>
2024-06-24 09:07:22 +0300

    fixed representation

--- week3/day5/DailyChallenge/DailyChallenge.py
+++ week3/day5/DailyChallenge/DailyChallenge.py
@@ -6,20 +6,41 @@
 # should have a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
 # should have a method called deal which deals a single card from the deck. After a card is dealt, it should be removed from the deck.
 
+import random
+
 
 class Card:
-    
-    def __init__(self):
-        self.ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-        self.suits = 'spades diamonds clubs hearts'.split()
+       
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
 
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
 
+        
 class Deck:
     def __init__(self):
+        ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+        suits = 'spades diamonds clubs hearts'.split()
         self.cards = [Card(rank, suit) for suit in suits for rank in ranks]
 
-    def shuffle():
+    def shuffle(self):
+        return random.shuffle(self.cards)
 
 
-    def deal():
-        
+    def deal(self):
+        return self.cards.pop() if self.cards else None
+      
+
+if __name__ == "__main__":
+    deck = Deck()
+    deck.shuffle()
+
+    print("5 random cards:")
+    for _ in range(5):
+        card = deck.deal()
+        if card:
+            print(card)
+        else:
+            print("No more cards to deal.")
