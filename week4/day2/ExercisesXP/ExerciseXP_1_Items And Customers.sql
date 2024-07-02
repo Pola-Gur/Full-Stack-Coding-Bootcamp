ce70225387a3143610619758d89e319e9025335b
Pola-Gur <pola.gur.il@gmail.com>
2024-07-02 11:46:45 +0300

    UPD for query history

--- week4/day2/ExercisesXP/ExerciseXP_1_Items And Customers.sql
+++ week4/day2/ExercisesXP/ExerciseXP_1_Items And Customers.sql
@@ -1,2 +1,5 @@
+-- select * from items order by price asc;
+-- select first_name, last_name from customers
+-- 	order by first_name asc limit 3;
 select last_name from customers
 	order by last_name desc;
--- week4/day2/ExercisesXP/ExerciseXP_2_Dvdrental Database.sql
+++ week4/day2/ExercisesXP/ExerciseXP_2_Dvdrental Database.sql
@@ -1,7 +1,31 @@
+select customer.first_name, customer.last_name, payment.amount, payment.payment_date 
+	from customer
+	inner join payment
+	on customer.customer_id= payment.customer_id
+	order by customer.customer_id;
+select * from inventory;
+select * from film;
+select film.film_id, film.title
+from film
+where not exists (
+	select 1
+	from inventory
+	where film.film_id = inventory.film_id
+	);
+select * from country;
+select * from city;
+select country.country_id, city.country_id, city.city, country.country
+	from city
+	full join country
+	on country.country_id = city.country_id;
+select country.country_id, city.city, country.country
+	from city
+	full join country
+	on country.country_id = city.country_id;
 select customer.customer_id, customer.first_name, customer.last_name, payment.amount, payment.payment_date, payment.staff_id
 	from customer
 	full join payment
 	on customer.customer_id = payment.customer_id
 	order by payment.staff_id;
-
-
+SELECT * FROM public.payment
+ORDER BY payment_id ASC 
