fa8df769ed3ea3fa74d1311f582ee86982da3f94
Pola-Gur <pola.gur.il@gmail.com>
2024-07-07 10:45:24 +0300

    exercise 4

--- week6/day2/ExercisesXP/ExercisesXP.js
+++ week6/day2/ExercisesXP/ExercisesXP.js
@@ -81,32 +81,31 @@ while (Number(number) < 10) {
 
 // 🌟 Exercise 4 : Building Management
 // Instructions:
-// const building = {
-//     numberOfFloors: 4,
-//     numberOfAptByFloor: {
-//         firstFloor: 3,
-//         secondFloor: 4,
-//         thirdFloor: 9,
-//         fourthFloor: 2,
-//     },
-//     nameOfTenants: ["Sarah", "Dan", "David"],
-//     numberOfRoomsAndRent:  {
-//         sarah: [3, 990],
-//         dan:  [4, 1000],
-//         david: [1, 500],
-//     },
-// }
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
 
-
-// Review About Objects
-// Copy and paste the above object to your Javascript file.
 
 // Console.log the number of floors in the building.
+console.log(building.numberOfFloors)
 
 // Console.log how many apartments are on the floors 1 and 3.
+console.log(Number(building.numberOfAptByFloor.firstFloor) + Number(building.numberOfAptByFloor.thirdFloor))
 
 // Console.log the name of the second tenant and the number of rooms he has in his apartment.
-
+console.log(building.nameOfTenants[1] + ', ' + building.numberOfRoomsAndRent.dan[0])
 // Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. If it is, than increase Dan’s rent to 1200.
 
 
