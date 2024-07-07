db31ef2721fc4478f64633764676b12e54feb988
Pola-Gur <pola.gur.il@gmail.com>
2024-07-07 21:41:49 +0300

    exercise 2 from Exercises XP

--- week5/day3/ExercisesXP/ExercisesXP.js
+++ week5/day3/ExercisesXP/ExercisesXP.js
@@ -48,27 +48,28 @@ function displayNumbersDivisible_with_divisor(divisor) {
 
 // 🌟 Exercise 2 : Shopping List
 // Instructions
-// const stock = { 
-//     "banana": 6, 
-//     "apple": 0,
-//     "pear": 12,
-//     "orange": 32,
-//     "blueberry":1
-// }  
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
 
-// const prices = {    
-//     "banana": 4, 
-//     "apple": 2, 
-//     "pear": 1,
-//     "orange": 1.5,
-//     "blueberry":10
-// } 
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
 // Add the stock and prices objects to your js file.
 
 // Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
 
-// Create a function called myBill() that takes no parameters.
+const shoppingList = ['banana', 'orange', 'apple']
 
+// Create a function called myBill() that takes no parameters.
 // The function should return the total price of your shoppingList. In order to do this you must follow these rules:
 // The item must be in stock. (Hint : check out if .. in)
 // If the item is in stock find out the price in the prices object.
@@ -76,6 +77,21 @@ function displayNumbersDivisible_with_divisor(divisor) {
 // Call the myBill() function.
 
 // Bonus: If the item is in stock, decrease the item’s stock by 1
+
+function myBill() {
+    let total_price = 0
+    for (let product of shoppingList) {
+        if (stock[product] > 0) {
+            total_price += prices[product]
+            stock[product] -= 1
+        }
+    }
+    console.log(total_price)
+}
+
+myBill()
+
+console.log(stock)
 
 
 // Exercise 3 : What’s In My Wallet ?
