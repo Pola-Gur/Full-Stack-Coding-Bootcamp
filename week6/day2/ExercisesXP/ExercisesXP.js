4df3711cab4a9a45f0011f906085cb4d40e1078a
Pola-Gur <pola.gur.il@gmail.com>
2024-07-07 10:37:10 +0300

    Exercise 2

--- week6/day2/ExercisesXP/ExercisesXP.js
+++ week6/day2/ExercisesXP/ExercisesXP.js
@@ -68,15 +68,9 @@ for (let color in colors) {
 // Prompt the user for a number.
 // Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
 
-number = prompt('Your number')
-console.log(typeof(number))
-
 // While the number is smaller than 10 continue asking the user for a new number.
 // Tip : Which while loop is more relevant for this situation?
 
-while (Number(number) < 10) {
-    number = prompt('Your number')
-}
 
 
 // 🌟 Exercise 4 : Building Management
