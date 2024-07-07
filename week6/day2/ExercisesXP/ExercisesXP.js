7d774f920a2da3bc2583a3522598fd68b7158c2f
Pola-Gur <pola.gur.il@gmail.com>
2024-07-07 10:29:09 +0300

    exercise 2 from Exercises XP

--- week6/day2/ExercisesXP/ExercisesXP.js
+++ week6/day2/ExercisesXP/ExercisesXP.js
@@ -50,3 +50,79 @@ for (let man of people) {
         break;
     }
 }
+
+// 🌟 Exercise 2 : Your Favorite Colors
+// Instructions
+// Create an array called colors where the value is a list of your five favorite colors.
+let colors = ['black', 'red', 'gray', 'lace', 'juniper']
+
+// Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+// Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+// Hint : create an array of suffixes to do the Bonus
+for (let color in colors) {
+    console.log('My №' + (Number(color)+1) + ' choice is ' + colors[color]);
+}
+
+// 🌟 Exercise 3 : Repeat The Question
+// Instructions
+// Prompt the user for a number.
+// Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+// While the number is smaller than 10 continue asking the user for a new number.
+// Tip : Which while loop is more relevant for this situation?
+
+
+// 🌟 Exercise 4 : Building Management
+// Instructions:
+// const building = {
+//     numberOfFloors: 4,
+//     numberOfAptByFloor: {
+//         firstFloor: 3,
+//         secondFloor: 4,
+//         thirdFloor: 9,
+//         fourthFloor: 2,
+//     },
+//     nameOfTenants: ["Sarah", "Dan", "David"],
+//     numberOfRoomsAndRent:  {
+//         sarah: [3, 990],
+//         dan:  [4, 1000],
+//         david: [1, 500],
+//     },
+// }
+
+
+// Review About Objects
+// Copy and paste the above object to your Javascript file.
+
+// Console.log the number of floors in the building.
+
+// Console.log how many apartments are on the floors 1 and 3.
+
+// Console.log the name of the second tenant and the number of rooms he has in his apartment.
+
+// Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. If it is, than increase Dan’s rent to 1200.
+
+
+// 🌟 Exercise 5 : Family
+// Instructions
+// Create an object called family with a few key value pairs.
+// Using a for in loop, console.log the keys of the object.
+// Using a for in loop, console.log the values of the object.
+
+
+// Exercise 6 : Rudolf
+// Instructions
+// const details = {
+//   my: 'name',
+//   is: 'Rudolf',
+//   the: 'reindeer'
+// }
+// Given the object above and using a for loop, console.log “my name is Rudolf the reindeer”
+
+
+// Exercise 7 : Secret Group
+// Instructions
+// const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+// A group of friends have decided to start a secret society. The society’s name will be the first letter of each of their names sorted in alphabetical order.
+// Hint: a string is an array of letters
+// Console.log the name of their secret society. The output should be “ABJKPS”
