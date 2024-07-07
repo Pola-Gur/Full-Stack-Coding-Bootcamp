91487e4a37e8984a2b8da361bb59016e99c37bf3
Pola-Gur <pola.gur.il@gmail.com>
2024-07-07 11:01:26 +0300

    Exercise 6

--- week6/day2/ExercisesXP/ExercisesXP.js
+++ week6/day2/ExercisesXP/ExercisesXP.js
@@ -130,13 +130,17 @@ for (let member in family) {
 
 // Exercise 6 : Rudolf
 // Instructions
-// const details = {
-//   my: 'name',
-//   is: 'Rudolf',
-//   the: 'reindeer'
-// }
+const details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'reindeer'
+}
 // Given the object above and using a for loop, console.log “my name is Rudolf the reindeer”
 
+for (let detail in details) {
+    console.log(detail)
+    console.log(details[detail])
+}
 
 // Exercise 7 : Secret Group
 // Instructions
