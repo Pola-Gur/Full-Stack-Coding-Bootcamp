3f9afd8da34e1df473d4fc337b72650bb3b57e1d
Pola-Gur <pola.gur.il@gmail.com>
2024-07-08 15:28:18 +0300

    dailychallenge day 1 week 6 about groceries

---
+++ week6/day1/DailyChallenge/main.js
@@ -0,0 +1,43 @@
+// Using this object :
+
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+// Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
+
+// Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method.
+function displayGroceries(array) {
+    array.forEach((element) => console.log(element));
+}
+displayGroceries(groceries.fruits)
+
+// Create another arrow function named cloneGroceries.
+// In the function, create a variable named user that is a copy of the client variable. (Tip : make the user variable equal to the client variable)
+// Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
+// In the function, create a variable named shopping that is equal to the groceries variable.
+// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
+// Change the value of the paid key to false. Will we also see this modification in the shopping object ? Why ?
+
+function cloneGroceries() {
+    let user = client;
+    client = "Betty";
+    console.log(`user: ${user}, client: ${client}`);
+    let shopping = groceries;
+    groceries.totalPrice = '35$';
+    groceries.other.paid = false;
+    console.log(groceries)
+    console.log(shopping)
+}
+
+// Invoke the cloneGroceries function.
+
+cloneGroceries()
