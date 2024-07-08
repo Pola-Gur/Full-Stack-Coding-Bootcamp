f0b1c92383fba002315f4398785440a9e5b577e4
Pola-Gur <pola.gur.il@gmail.com>
2024-07-08 19:55:29 +0300

    drafts for day 4 of week 4

---
+++ week4/day4/ExercisesXP/menu_editor.py
@@ -0,0 +1,62 @@
+# In the file menu_item.py, create a new class called MenuItem, the attributes should be the name and price of each item.
+# Create several methods (save, delete, update) these methods will allow a user to save, delete and update items from the Menu_Items table. The update method can update the name as well as the price of an item.
+
+class MenuItem:
+    name = 
+    price =
+
+    def save():
+        ...
+
+    def delete():
+        ...
+
+    def update():
+        ...
+
+
+# Part 2
+
+# Create a file called menu_editor.py , which will have the following functions:
+# show_user_menu() - this function should display the program menu (not the restaurant menu!), and ask the user to :
+def show_user_menu():
+    program_menu = """Our action menu:
+                  View an Item as V
+                  Add an Item as A
+                  Delete an Item as D
+                  Update an Item as U
+                  Show the Menu as S """
+# View an Item (V)
+# Add an Item (A)
+# Delete an Item (D)
+# Update an Item (U)
+# Show the Menu (S)
+# Call the appropriate function that matches the user’s input.
+
+# add_item_to_menu() - this function should ask the user to input the item’s name and price. This function will not interact with the menu itself, but simply create a MenuItem object and call the appropriate function from the MenuItem object.
+# If the item was added successfully print a message which states: item was added successfully.
+
+def add_item_to_menu():
+    action = input("Choose your action with one letter\n")
+    print(f'{action} was added successfully')
+
+# remove_item_from_menu()- this function should ask the user to input the name of the item they want to remove from the restaurant’s menu. This function will not interact with the menu itself, but simply create a MenuItem object and call the appropriate function from the MenuItem object.
+# If the item was deleted successfully – print a message to let the user know this was completed.
+# If not – print a message which states that there was an error.
+
+def remove_item_from_menu():
+    ...
+
+# update_item_from_menu()- this function should ask the user to input the name and price of the item they want to update from the restaurant’s menu, as well as to input the name and price they want to change them with. This function will not interact with the menu itself, but simply create a MenuItem object and call the appropriate function from the MenuItem object.
+# If the item was updated successfully – print a message to let the user know this was completed.
+# If not – print a message which states that there was an error.
+
+def update_item_from_menu():
+    ...
+
+# show_restaurant_menu() - print the restaurant’s menu.
+
+def show_restaurant_menu():
+    print("this place for restaurant's menu")    
+    
+# When the user chooses to exit the program, display the restaurant menu and exit the program.
---
+++ week4/day4/ExercisesXP/menu_manager.py
@@ -0,0 +1,13 @@
+# In the file menu_manager.py, create a new class called MenuManager
+# Create a Class Method called get_by_name that will return a single item from the Menu_Items table depending on it’s name, if an object is not found (there is no item matching the name in the get_by_name method) return None.
+
+# Create a Class Method called all_items which will return a list of all the items from the Menu_Items table.
+
+# Codebox:
+
+# item = MenuItem('Burger', 35)
+# item.save()
+# item.delete()
+# item.update('Veggie Burger', 37)
+# item2 = MenuManager.get_by_name('Beef Stew')
+# items = MenuManager.all()
