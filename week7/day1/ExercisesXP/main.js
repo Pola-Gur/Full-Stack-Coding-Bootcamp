b03b5d5ad071344b32ac1ecb6708f4800857a82a
Pola-Gur <pola.gur.il@gmail.com>
2024-07-14 21:07:36 +0300

    exercise 1

---
+++ week7/day1/ExercisesXP/main.js
@@ -0,0 +1,43 @@
+// 🌟 Exercise 1 : Comparison
+// Instructions
+// Create a function called compareToTen(num) that takes a number as an argument.
+// The function should return a Promise:
+// the promise resolves if the argument is less than or equal to 10
+// the promise rejects if argument is greater than 10.
+
+function compareToTen(num) {
+    return new Promise((resolve, reject) => {
+        if (num <= 10) {
+            resolve(`The number ${num} is less than or equal to 10`);
+        } else {
+            reject(`The number ${num} is greater than 10`);
+        }
+    });
+}
+
+// Test:
+
+//In the example, the promise should reject
+compareToTen(15)
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+//In the example, the promise should resolve
+compareToTen(8)
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+
+
+// 🌟 Exercise 2 : Promises
+// Instructions
+// Create a promise that resolves itself in 4 seconds and returns a “success” string.
+
+
+// 🌟 Exercise 3 : Resolve & Reject
+// Instructions
+// Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
+// Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
+
+
+// Exercise 4: Quizz - Not Mandatory
+// Follow this tutorial and do the quizz until the page called “a few tricks with promises”.
