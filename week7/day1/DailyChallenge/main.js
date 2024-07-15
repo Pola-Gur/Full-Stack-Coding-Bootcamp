6344118bb450c6990753d4f0296f9ae52229a746
Pola-Gur <pola.gur.il@gmail.com>
2024-07-15 09:23:54 +0300

    draft for second challenge

--- week7/day1/DailyChallenge/main.js
+++ week7/day1/DailyChallenge/main.js
@@ -60,53 +60,54 @@ makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
 
 
 // 2nd Daily Challenge
-// const morse = `{
-//   "0": "-----",
-//   "1": ".----",
-//   "2": "..---",
-//   "3": "...--",
-//   "4": "....-",
-//   "5": ".....",
-//   "6": "-....",
-//   "7": "--...",
-//   "8": "---..",
-//   "9": "----.",
-//   "a": ".-",
-//   "b": "-...",
-//   "c": "-.-.",
-//   "d": "-..",
-//   "e": ".",
-//   "f": "..-.",
-//   "g": "--.",
-//   "h": "....",
-//   "i": "..",
-//   "j": ".---",
-//   "k": "-.-",
-//   "l": ".-..",
-//   "m": "--",
-//   "n": "-.",
-//   "o": "---",
-//   "p": ".--.",
-//   "q": "--.-",
-//   "r": ".-.",
-//   "s": "...",
-//   "t": "-",
-//   "u": "..-",
-//   "v": "...-",
-//   "w": ".--",
-//   "x": "-..-",
-//   "y": "-.--",
-//   "z": "--..",
-//   ".": ".-.-.-",
-//   ",": "--..--",
-//   "?": "..--..",
-//   "!": "-.-.--",
-//   "-": "-....-",
-//   "/": "-..-.",
-//   "@": ".--.-.",
-//   "(": "-.--.",
-//   ")": "-.--.-"
-// }`
+const morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`
+
 // Create three functions. The two first functions should return a promise..
 
 // The first function is named toJs():
@@ -114,8 +115,17 @@ makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
 // if the morse javascript object is empty, throw an error (use reject)
 // else return the morse javascript object (use resolve)
 
+function toJS(morse) {
+    return new Promise((resolve, reject) {
+        if (morse) {
+            const morseJS = JSON.parse(morse);
+            resolve(morse);
+        } else {
+            return reject("Empty given element")
+        }
+    });
+}
 // The second function called toMorse(morseJS), takes one argument: the new morse javascript object.
-
 // This function asks the user for a word or a sentence.
 // if the user entered a character that doesn’t exist in the new morse javascript object, throw an error. (use reject)
 // else return an array with the morse translation of the user’s word.
@@ -123,9 +133,30 @@ makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
 // if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object
 
 
+function toMorse(morseJS) {
+    const user_input = promt("Enter a word or a sentence");
+    let translation = [];
+    return new Promise((resolve, reject) {
+        morseJS.forEach(element => {
+            if (morseJS.includes(element)) {
+                translation.push(morseJS[element]);
+            } else {
+                return reject("The string contains invalid characters")
+            }
+            resolve(morseJS)
+    });
+    });
+};    
+
+
 // The third function called joinWords(morseTranslation), takes one argument: the morse translation array
 
 // this function joins the morse translation by using line break and display it on the page (ie. On the DOM)
+
+function joinWords(translation) {
+    console.log(translation.join("-"));
+    
+}
 
 // Chain the three functions.
 // Example:
