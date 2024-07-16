7a1254c3f85c6ce51ba283df8e2a08b7817d48f9
Pola-Gur <pola.gur.il@gmail.com>
2024-07-16 15:16:36 +0300

    draft animation 1

---
+++ week5/day4/ExercisesXP_Animations/ExerciseXP_Animations1.js
@@ -0,0 +1,34 @@
+// 🌟 Exercise 1: Timer
+// Instructions
+// Using this HTML code:
+
+// Part I
+// In your Javascript file, using setTimeout, call a function after 2 seconds.
+// The function will alert “Hello World”.
+
+function setTime() {
+    setTimeout(function(){
+        alert("Hello World");
+    }, 2000)
+}
+setTime()
+
+// Part II
+// In your Javascript file, using setTimeout, call a function after 2 seconds.
+// The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
+
+function addParagraph() {
+    setTimeout(function(){
+        const container = document.getElementById("container");
+        const p = document.createElement("p");
+        p.innerHTML = "Hello World";
+        container.appendChild(p)
+    }, 2000)
+}
+addParagraph()
+
+// Part III
+// In your Javascript file, using setInterval, call a function every 2 seconds.
+// The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
+// The interval will be cleared (ie. clearInterval), when the user will click on the button.
+// Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
