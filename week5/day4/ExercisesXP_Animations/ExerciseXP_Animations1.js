2dda814b194928e134db38ec6b2b65a4b3e2735e
Pola-Gur <pola.gur.il@gmail.com>
2024-07-16 15:19:02 +0300

    draft part animation 1

--- week5/day4/ExercisesXP_Animations/ExerciseXP_Animations1.js
+++ week5/day4/ExercisesXP_Animations/ExerciseXP_Animations1.js
@@ -32,3 +32,7 @@ addParagraph()
 // The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
 // The interval will be cleared (ie. clearInterval), when the user will click on the button.
 // Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
+
+function setIner() {
+    
+}
