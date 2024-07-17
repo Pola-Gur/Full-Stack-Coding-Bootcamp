800f03bc3db5bfd14d49405661ec2594f41cdd64
Pola-Gur <pola.gur.il@gmail.com>
2024-07-17 19:47:33 +0300

    color is changing with click!!!

--- week5/day5/ExercisesXP/main.js
+++ week5/day5/ExercisesXP/main.js
@@ -4,23 +4,22 @@ const colors = document.querySelectorAll("#colors div");
 
 function checker(event) {
     console.log(event.target.id);
-    let currentColor = event.target.style.backgroundColor;
+    currentColor = event.target.style.backgroundColor;
     console.log(currentColor)
-    return currentColor
 }
 
 colors.forEach(color => {
     color.addEventListener("click", checker);
 });
 
-
-
-const square = document.querySelectorAll("#field div");
+// const square = document.querySelectorAll("#field div");
 
 const main = document.querySelector("main");
-main.addEventListener("mouseover", function(event) {
+main.addEventListener("click", function(event) {
+    if (event.target !== main) { 
     event.target.style.backgroundColor = currentColor;
-})
+    }
+});
 
 
 const clear = document.getElementById("clear");
