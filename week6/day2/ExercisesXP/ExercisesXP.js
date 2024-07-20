f2ade9065507e6f64b4471dc04b42f93874bf0a0
Pola-Gur <pola.gur.il@gmail.com>
2024-07-20 20:43:16 +0300

    all done

--- week6/day2/ExercisesXP/ExercisesXP.js
+++ week6/day2/ExercisesXP/ExercisesXP.js
@@ -108,11 +108,19 @@ console.log(epic.reduce(redFunc))
 // Instructions
 // Using this object:
 
-// const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
-//                {name: "Liam", course: "Computer Science", isPassed: false}, 
-//                {name: "Jenner", course: "Information Technology", isPassed: true}, 
-//                {name: "Marco", course: "Robotics", isPassed: true}, 
-//                {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
-//                {name: "Jamie", course: "Big Data", isPassed: false}];
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+               {name: "Liam", course: "Computer Science", isPassed: false}, 
+               {name: "Jenner", course: "Information Technology", isPassed: true}, 
+               {name: "Marco", course: "Robotics", isPassed: true}, 
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
 // Using the filter() method, create a new array, containing the students that passed the course.
-// Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+
+const bestStudents = students.filter(student => student.isPassed === true);
+console.log(bestStudents)
+
+// Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name
+// (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+
+bestStudents.forEach(stud => console.log(`Good job ${stud.name} you passed the course in Robotics`))
