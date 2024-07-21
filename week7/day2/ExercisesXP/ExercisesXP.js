bf3b4bc89f6ff14b27dbc9ec353ef6acdc2e8b07
Pola-Gur <pola.gur.il@gmail.com>
2024-07-21 15:57:52 +0300

    done

--- week7/day2/ExercisesXP/ExercisesXP.js
+++ week7/day2/ExercisesXP/ExercisesXP.js
@@ -77,18 +77,21 @@ fetchPractise()
 // Instructions
 // Analyse the code provided below - what will be the outcome?
 
-// function resolveAfter2Seconds() {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve('resolved');
-//         }, 2000);
-//     });
-// }
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
 
-// async function asyncCall() {
-//     console.log('calling');
-//     let result = await resolveAfter2Seconds();
-//     console.log(result);
-// }
+async function asyncCall() {
+    console.log('calling');
+    let result = await resolveAfter2Seconds();
+    console.log(result);
+}
 
-// asyncCall();
+asyncCall();
+
+
+// resolved
