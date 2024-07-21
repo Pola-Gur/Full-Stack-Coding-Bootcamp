9e17bdb8777489839f1c11afbdc51b11c59fae18
Pola-Gur <pola.gur.il@gmail.com>
2024-07-21 21:10:57 +0300

    done DailyChallenge

--- week7/day2/DailyChallenge/DailyChallenge.js
+++ week7/day2/DailyChallenge/DailyChallenge.js
@@ -5,3 +5,42 @@
 // The gif should be appended with a DELETE button next to it. Hint : to find the URL of the gif, look for the sub-object named “images” inside the data you receive from the API.
 // Allow the user to delete a specific gif by clicking the DELETE button.
 // Allow the user to remove all of the GIFs by clicking a DELETE ALL button.
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('Form');
+    const container = document.getElementById('container');
+    const deleteAll = document.getElementById('deleteAll');
+    const apiKey = 'hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'; 
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input");
+        const category = input.value;
+        const fullUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${category}`;
+
+
+    fetch(fullUrl)
+        .then(response => response.json())
+        .then(data => {
+            const gifUrl = data.data.images.original.url;  
+            const gifElem = document.createElement("div");
+            const imgElem = document.createElement("img");
+            imgElem.src = gifUrl; 
+            gifElem.appendChild(imgElem); 
+            
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => {
+                gifElem.remove();
+            });
+            gifElem.appendChild(deleteButton);
+            container.appendChild(gifElem);
+        })
+        .catch(error => console.error('Error >>', error)); 
+        });
+
+    deleteAll.addEventListener('click', () => {
+    container.innerHTML = '';
+    });
+});
