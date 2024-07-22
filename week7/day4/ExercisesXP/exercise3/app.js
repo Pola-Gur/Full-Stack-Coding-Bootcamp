8a78f841b26e0a578bcaac7825820e324130d4c3
Pola-Gur <pola.gur.il@gmail.com>
2024-07-22 21:10:53 +0300

    draft exercise 3

---
+++ week7/day4/ExercisesXP/exercise3/app.js
@@ -0,0 +1,5 @@
+// import the functions from the fileManager.js module.
+
+// Use the imported functions to read the content of the “Hello World.txt” text file and then write to the “Bye World.txt” with the content “Writing to the file”.
+
+// Run app.js and verify that the file reading and writing operations are successful.
---
+++ week7/day4/ExercisesXP/exercise3/fileManager.js
@@ -0,0 +1,5 @@
+// define a module that exports functions for reading and writing files.
+// Export functions named readFile and writeFile.
+// Implement the readFile function to read the content of a specified file using the fs module.
+// Implement the writeFile function to write content to a specified file using the fs module.
+
