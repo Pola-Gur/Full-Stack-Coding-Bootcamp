25c417d8527cb2b69ba3361042ede8d58fa475a8
Pola-Gur <pola.gur.il@gmail.com>
2024-07-23 08:03:33 +0300

    exercise 3 donr

--- week7/day4/ExercisesXP/exercise3/app.js
+++ week7/day4/ExercisesXP/exercise3/app.js
@@ -3,3 +3,11 @@
 // Use the imported functions to read the content of the “Hello World.txt” text file and then write to the “Bye World.txt” with the content “Writing to the file”.
 
 // Run app.js and verify that the file reading and writing operations are successful.
+
+import { readFile, writeFile } from "./fileManager.js";
+
+
+const textFromHelloWorld = readFile("Hello World.txt");
+console.log(textFromHelloWorld);
+
+writeFile("Bye World.txt", "Writing to the file");
--- week7/day4/ExercisesXP/exercise3/fileManager.js
+++ week7/day4/ExercisesXP/exercise3/fileManager.js
@@ -3,3 +3,23 @@
 // Implement the readFile function to read the content of a specified file using the fs module.
 // Implement the writeFile function to write content to a specified file using the fs module.
 
+import fs from "fs";
+
+export function readFile(fileName) {
+    try {
+        const data = fs.readFileSync(fileName, "utf-8");
+        return data;
+    } catch (err) {
+        console.error("Error reading file")
+    }
+}
+
+export function writeFile(fileName, newText) {
+    try {
+        fs.writeFileSync(fileName, newText);
+        console.log("Write operation complete.");
+    } catch (err) {
+        console.error("Error writing file", err);
+    }
+};
+        
