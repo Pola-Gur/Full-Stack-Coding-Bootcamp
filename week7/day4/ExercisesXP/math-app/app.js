4b3b1bbcff2c83fde029d1efa1dc2b0bcff5ba45
Pola-Gur <pola.gur.il@gmail.com>
2024-07-23 10:34:58 +0300

    exercise 5 done

---
+++ week7/day4/ExercisesXP/math-app/app.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+import { addFunc, multiply } from "./math.js"
+
+console.log(addFunc(6, 3));
+console.log(multiply(2, 9));
+console.log(_.sum([1, 2, 3, 4]));
---
+++ week7/day4/ExercisesXP/math-app/math.js
@@ -0,0 +1,14 @@
+
+function addFunc(a, b) {
+    return a+b;
+}
+
+
+function multiply(a, b) {
+    return a*b;
+}
+
+export {
+    addFunc,
+    multiply
+}
