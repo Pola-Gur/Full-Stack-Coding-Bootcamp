944f6dbabe12ab778ed1ca9417c432dd53acad89
Pola-Gur <pola.gur.il@gmail.com>
2024-07-23 22:50:08 +0300

    both GET method done in the first exercise

--- week7/day5/ExercisesXP/ExercisesXP.js
+++ week7/day5/ExercisesXP/ExercisesXP.js
@@ -7,18 +7,7 @@
 // Inside the blog-api directory, open a terminal and run npm init to initialize a new Node.js project. Follow the prompts to set up your project.
 // Install the express package by running npm install express in the terminal.
 // Create a file named server.js.
-// In server.js, require the express package and set up an Express app.
-// Create a data array to simulate a database. Each item in the array should represent a blog post with properties like id, title, and content.
-
-// Implement the following routes using Express:
-// GET /posts: Return a list of all blog posts.
-// GET /posts/:id: Return a specific blog post based on its id.
-// POST /posts: Create a new blog post.
-// PUT /posts/:id: Update an existing blog post.
-// DELETE /posts/:id: Delete a blog post.
-
-// Implement error handling for invalid routes and server errors.
-// Start the Express app and listen on a specified port (e.g., 3000).
+// 
 
 
 // 🌟 Exercise 2: Building A Basic CRUD API With Express.Js
--- week7/day5/ExercisesXP/blog-api/server.js
+++ week7/day5/ExercisesXP/blog-api/server.js
@@ -1,6 +1,14 @@
+// In server.js, require the express package and set up an Express app.
+// Create a data array to simulate a database. Each item in the array should represent a blog post with properties like id, title, and content.
+
 const express = require("express");
 
-let data = [
+const app = express();
+
+const PORT = 5003;
+
+
+let dataBasePosts = [
     {
     "id": 1,
     "title": "Introduction",
@@ -27,3 +35,36 @@ let data = [
     "content": "smth about wonderful and cheap food, beautiful people and historical placec"
     },
 ]
+
+
+// Implement the following routes using Express:
+// GET /posts: Return a list of all blog posts.
+app.get("/posts", (req, res) => {
+    res.send(dataBasePosts);
+});
+
+// GET /posts/:id: Return a specific blog post based on its id.
+app.get("/posts/:id", (req, res) => {
+    console.log(req.params);
+    const { id } = req.params; 
+
+    const onePost = dataBasePosts.find((item) => item.id == id);
+
+    if (!onePost) return res.status(404).json({msg: `ID = ${id} not found`});
+
+    res.send(onePost);
+});
+// POST /posts: Create a new blog post.
+// app.post()
+// // PUT /posts/:id: Update an existing blog post.
+// app.put()
+// // DELETE /posts/:id: Delete a blog post.
+// app.delete()
+
+// Implement error handling for invalid routes and server errors.
+// Start the Express app and listen on a specified port (e.g., 3000).
+
+
+app.listen(PORT, () => {
+    console.log(`Run on ${PORT}`);
+});
