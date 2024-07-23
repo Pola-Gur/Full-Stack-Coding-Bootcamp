0c67c4b2988148cfe2734631f201c946f36b3dc3
Pola-Gur <pola.gur.il@gmail.com>
2024-07-23 14:40:52 +0300

    draft for 1 exercise in exercises XP

---
+++ week7/day5/ExercisesXP/blog-api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+let data = [
+    {
+    "id": 1,
+    "title": "Introduction",
+    "content": "Shalom leculam, followers!", 
+    },
+    {
+    "id": 2,
+    "title": "Funny story",
+    "content": "Here is true ha-ha, hehe and ho-ho!",
+    },
+    {
+    "id": 3,
+    "title": "Homeland",
+    "content": "Long touching story with open ending",
+    },
+    {
+    "id": 4,
+    "title": "Child dream",
+    "content": "Astronaut? Doctor? Teacher? Cashier?"
+    },
+    {
+    "id": 5,
+    "title": "Eurotrip",
+    "content": "smth about wonderful and cheap food, beautiful people and historical placec"
+    },
+]
