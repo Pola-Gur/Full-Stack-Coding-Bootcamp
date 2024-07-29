9d148c4db54defc9e8cc4fc28b78d87e942d82b9
Pola-Gur <pola.gur.il@gmail.com>
2024-07-29 09:53:25 +0300

    done with options

--- week7/day5/DailyChallenge/public/script.js
+++ week7/day5/DailyChallenge/public/script.js
@@ -1,17 +1,53 @@
 const url = "http://localhost:3001/emojis"
+const submitUrl = "http://localhost:3001/submit";
 
 const randomEmoji = document.getElementById("emoji")
+const answerDIV = document.getElementById("options");
+const feedbackDiv = document.getElementById("feedback");
 
-const emojis = () => {
+
+let currentEmojiName = '';
+
+
+const loadEmoji = () => {
     fetch(url)
     .then((res) => res.json())
     .then((emojis) => {
         // console.log(emojis);
         // console.log(emojis.randomEmoji);
         randomEmoji.innerHTML = emojis.randomEmoji.emoji
+        currentEmojiName = emojis.randomEmoji.name;
+        answerDIV.innerHTML = '';
+
+        emojis.options.forEach(option => {
+            const button = document.createElement("button");
+            button.textContent = option.name;
+            button.onclick = () => submitGuess(option.name);
+            answerDIV.appendChild(button);
+        });
     })
     .catch((e) => {
         console.log(e);
     });
 };
-emojis()
+
+const submitGuess = (guess) => {
+    fetch(submitUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ guess, correctAnswer: currentEmojiName }),
+    })
+        .then(res => res.json())
+        .then(data => {
+            feedbackDiv.innerHTML = data.isCorrect ? "Right!" : "Wrong!";
+            setTimeout(() => {
+                feedbackDiv.innerHTML = '';
+                loadEmoji();
+            }, 2000);
+        });
+};
+
+
+loadEmoji()
--- week7/day5/DailyChallenge/server.js
+++ week7/day5/DailyChallenge/server.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const { emojis } = require("./config/emojis.js");
+const cors = require("cors");
 
 const app = express()
+app.use(cors());
+app.use(express.json()); 
 
 const PORT = 3001;
+
+
+
+app.use('/', express.static(__dirname + "/public"));
+
+const getRandomEmojiWithOptions = () => {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    const randomEmoji = emojis[randomIndex];
+    
+    const incorrectOptions = emojis.filter(e => e.name !== randomEmoji.name)
+                                    .sort(() => 0.5 - Math.random())
+                                    .slice(0, 3);
+    const options = [randomEmoji, ...incorrectOptions].sort(() => 0.5 - Math.random());
+
+    return { randomEmoji, options };
+};
+
+app.get("/emojis", (req, res) => {
+    const { randomEmoji, options } = getRandomEmojiWithOptions();
+    res.json({ randomEmoji, options });
+});
+
+app.post('/submit', (req, res) => {
+    const { guess, correctAnswer } = req.body;
+    const isCorrect = guess === correctAnswer;
+    res.json({ isCorrect });
+});
+
 
 app.listen(PORT, ()=> {
     console.log("run on ", PORT);
 })
-
-app.use('/', express.static(__dirname + "/public"));
-
-app.get("/emojis", (req, res) => {
-    const randomIndex = Math.floor(Math.random() * emojis.length)
-    const randomEmoji = emojis[randomIndex];
-
-    res.json({randomEmoji});
-});
-
