18efee9964eadd952bf81e15e96b5801f09afb77
Pola-Gur <pola.gur.il@gmail.com>
2024-07-31 16:28:09 +0300

    everything done

--- week7/day5/ExercisesXP/crud-api/app.js
+++ week7/day5/ExercisesXP/crud-api/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const { fetchPosts} = require("./data/dataService.js")
 
 const PORT = 5000;
 app.use(express.json());
-app.use(express.static('public'));
+
 
 app.listen(PORT, () => {
     console.log(`Run on ${PORT}`);
+});
+
+
+app.get('/posts', (req, res) => {
+    fetchPosts()
+        .then(posts => {
+            res.json(posts);
+            console.log("Data has been successfully retrieved and sent as a response");
+        })
+        .catch(error => {
+            res.status(500).send('Error retrieving data');
+        });
 });
--- week7/day5/ExercisesXP/crud-api/data/dataService.js
+++ week7/day5/ExercisesXP/crud-api/data/dataService.js
@@ -1,12 +1,13 @@
 const axios = require("axios")
 
 function fetchPosts() {
-    return axios.getAdapter("https://jsonplaceholder.typicode.com/posts")
+    return axios.get("https://jsonplaceholder.typicode.com/posts")
         .then(response => {
             return response.data;
-        } catch(error) =>
+        }) .catch(error => {
             console.error('Error:', error);
             throw error;
-)};
+        });
+};
 
 module.exports = { fetchPosts };
