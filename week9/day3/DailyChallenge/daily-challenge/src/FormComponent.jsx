6cf01618f80d72f2a989998f5ef094b1d3174a94
Pola-Gur <pola.gur.il@gmail.com>
2024-08-06 23:35:33 +0300

    draft

---
+++ week9/day3/DailyChallenge/daily-challenge/src/FormComponent.jsx
@@ -0,0 +1,39 @@
+
+
+const FormCOmponent = {
+
+    return (
+        <form> 
+            <input placeholder="First Name"/>
+            <input placeholder="Last Name"/>
+            <input placeholder="Age"/>
+
+            <label>
+                <input type="radio"></input>Male
+            </label>
+            <label>
+                <input type="radio"></input>Female
+            </label>
+
+            <select>
+            <option value="">Please Choose a destination</option>
+                <option>Thailand</option>
+                <option>Japan</option>
+                <option>Brazil</option>
+            </select>
+            
+            <label>Dietary restrictions:</label>
+            <label>
+                <input type="checkbox"/>Nuts free
+            </label
+            <label
+                <input type="checkbox"/>Lactose free
+            </label
+            <label
+                <input type="checkbox"/>Vegan
+            </label>
+            
+
+        </form>
+    )
+}
