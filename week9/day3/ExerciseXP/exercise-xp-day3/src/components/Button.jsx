32e20e8db6d349e54a2512747dd1ab9c6dd39ef7
Pola-Gur <pola.gur.il@gmail.com>
2024-08-07 18:06:33 +0300

    draft

--- week9/day3/ExerciseXP/exercise-xp-day3/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button= ({setCount, count}) => {
-    // console.log(props);
-    return (
-        <>
-            <button onClick={()=> setCount(count +1)}> + </button>
-        </>
-    )
-}
-export default Button;
---
+++ week9/day3/ExercisesXP/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import React from 'react';
+import './App.css'
+
+
+class BuggyCounter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState((state) => {
+      return {count: state.count + 1}
+    })
+  }
+
+  render() {
+    // const handleClick = () => {
+    //   this.setState((state) => {
+    //     return {count: state.count + 1}
+    //   })
+    // }
+    if (this.state.count >5) {
+      throw new Error("I crashed!")
+    }
+
+    return (
+        <button onClick={this.handleClick}>
+          count is {this.state.count}
+        </button>
+    )
+  }
+}
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <BuggyCounter/>
+      </div>
+
+    </>
+  )
+}
+
+// export default BuggyCounter;
+export default App
+
+
+// <button onClick={() => setCount((count) => count + 1)}>
+// count is {count}
+// </button>
