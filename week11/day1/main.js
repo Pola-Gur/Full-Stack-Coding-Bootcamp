3c5b290348a5d37d7f750550d20737ed1f5b40ae
Pola-Gur <pola.gur.il@gmail.com>
2024-08-11 15:08:18 +0300

    1-3 exercises

---
+++ week11/day1/main.js
@@ -0,0 +1,8 @@
+// ex 1 Create a TypeScript program that logs the message “Hello, World!” to the console.
+console.log("Hello, World!");
+// ex 2 Define a variable age of type number and a variable name of type string, and log them to the console.
+var age = 23;
+var nameExercise2 = "Zvi";
+console.log(age, nameExercise2);
+// ex 3 Define a variable id that can be either a string or a number.
+var id = 18;
---
+++ week11/day1/main.ts
@@ -0,0 +1,10 @@
+// ex 1 Create a TypeScript program that logs the message “Hello, World!” to the console.
+console.log("Hello, World!")
+
+// ex 2 Define a variable age of type number and a variable name of type string, and log them to the console.
+let age: number = 23;
+let nameExercise2: string = "Zvi";
+console.log(age, nameExercise2)
+
+// ex 3 Define a variable id that can be either a string or a number.
+let id: string | number = 18;
