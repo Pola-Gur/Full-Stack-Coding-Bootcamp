f3c1a2f810ba75e76f1d95fdc7660e8a3ab1e694
Pola-Gur <pola.gur.il@gmail.com>
2024-08-12 16:13:20 +0300

    deleted useless details

--- week10/day1/ExercisesXP/exercise1/src/App.jsx
+++ week10/day1/ExercisesXP/exercise1/src/App.jsx
@@ -19,7 +19,6 @@ function App() {
       <div className="App">
         <h1>Theme Switcher Example</h1>
         <ThemeSwitcher />
-        {/* Other components here */}
       </div>
     </ThemeProvider>
   );
