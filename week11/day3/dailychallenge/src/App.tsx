ee8f725bc0dc9521f1fa1ebe077ab3049f5cba2d
Pola-Gur <pola.gur.il@gmail.com>
2024-08-13 19:49:04 +0300

    done

---
+++ week11/day3/dailychallenge/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TodoApp from './components/TodoList'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <TodoApp/>
+    </>
+  )
+}
+
+export default App
